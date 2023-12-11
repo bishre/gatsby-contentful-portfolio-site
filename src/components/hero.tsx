8ca1cd1c259abe7521f08756a5e20c781258f78d
bibhor shrestha <bibhorshrestha@gmail.com>
2023-12-11 02:05:40 +0200

    Add finnish page (#5)

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,9 +5,7 @@ import { useInView } from 'react-intersection-observer';
 
 type Props = {
   title: string;
-  description: {
-    description: string;
-  }
+  description: string;
   heroImage: {
     file: {
       url: string;
@@ -20,13 +18,13 @@ const Hero = ({ title, description, heroImage }: Props) => {
     triggerOnce: true, // Trigger animation only once
     threshold: 1
   });
-  const isMobile = typeof window !== "undefined" && window.innerWidth < 1024 
+  const isMobile = typeof window !== "undefined" && window.innerWidth < 1024
   return (
     <div className='relative'>
       <div className='absolute w-50 top-1/2 text-white text-3xl transform -translate-y-1/2 mx-16 z-10'>
         <div ref={ref} className={`${inView ? 'transform translate-y-0 transition-all opacity-100' : 'transform translate-y-1/4 opacity-0'} duration-500 ease-in-out`}>
           <h2 className="text-2xl md:text-6xl lg:my-8">{title} (Beev)</h2>
-          <p className={`bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 inline-block text-transparent bg-clip-text text-sm md:text-lg lg:text-2xl ${inView ? 'opacity-100' : 'opacity-0'} transition transform-all duration-500 ease-in`}>{description.description}</p>
+          <p className={`bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 inline-block text-transparent bg-clip-text text-sm md:text-lg lg:text-2xl ${inView ? 'opacity-100' : 'opacity-0'} transition transform-all duration-500 ease-in`}>{description}</p>
         </div>
       </div>
       <RatioContainer width={isMobile ? 4 : 16} height={isMobile ? 5 : 9}>
