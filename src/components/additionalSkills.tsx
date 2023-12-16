9cd809bea91cee37e9f7be428bbfba0c716615b7
bishre <bibhorshrestha@gmail.com>
2023-12-16 02:07:17 +0200

    Gsap

--- src/components/additionalSkills.tsx
+++ src/components/additionalSkills.tsx
@@ -1,20 +1,39 @@
-import React from 'react'
+import gsap from 'gsap';
+import React, { useEffect } from 'react'
+import { useInView } from 'react-intersection-observer';
 
 type Props = {}
 
 const AdditionalSkills = (props: Props) => {
+  const [ref, inView] = useInView({
+    triggerOnce: true, // Trigger animation only once
+    threshold: 1
+  });
+
+  useEffect(() => {
+    if (!inView) return
+    const textElements = document.querySelectorAll(".stagger")
+    gsap.from(textElements, {
+      duration: 0.4,
+      opacity: 0,
+      y: 50,
+      stagger: 0.1,
+      ease: "power2.out"
+    })
+  }, [inView])
+
   return (
-    <div>
+    <div ref={ref}>
       <h2 className='text-3xl my-12'>Additional technologies and Skills</h2>
-      <ul className='grid grid-cols-3 mx-60'>
-        <li className='list-styles'><p>Git</p></li>
-        <li className='list-styles'><p>Linux</p></li>
-        <li className='list-styles'><p>B1 Finnish</p></li>
-        <li className='list-styles'><p>MobX</p></li>
-        <li className='list-styles'><p>Python</p></li>
-        <li className='list-styles'><p>Django</p></li>
-        <li className='list-styles'><p>A11y</p></li>
-        <li className='list-styles'><p>Virtual Private Servers</p></li>
+      <ul className={`grid grid-cols-3 mx-60 ${inView ? 'opacity-100' : 'opacity-0'}`}>
+        <li className='list-styles stagger'><p>Git</p></li>
+        <li className='list-styles stagger'><p>Linux</p></li>
+        <li className='list-styles stagger'><p>B1 Finnish</p></li>
+        <li className='list-styles stagger'><p>MobX</p></li>
+        <li className='list-styles stagger'><p>Python</p></li>
+        <li className='list-styles stagger'><p>Django</p></li>
+        <li className='list-styles stagger'><p>A11y</p></li>
+        <li className='list-styles stagger'><p>Virtual Private Servers</p></li>
       </ul>
     </div>
   )
