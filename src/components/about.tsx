8304ec866cf3b4f3b774a96e63dab9ab3b200910
bishre <bibhorshrestha@gmail.com>
2023-12-20 01:02:00 +0200

    About parallax

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,10 +1,13 @@
 import { BLOCKS } from '@contentful/rich-text-types'
 import { graphql, useStaticQuery } from 'gatsby'
 import { renderRichText } from 'gatsby-source-contentful/rich-text'
-import React from 'react'
+import React, { useEffect } from 'react'
 import ComponentWrapper from './componentWrapper'
 import { useInView } from 'react-intersection-observer';
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import gsap from 'gsap'
 
+gsap.registerPlugin(ScrollTrigger)
 type Props = {}
 
 const About = (props: Props) => {
@@ -12,6 +15,26 @@ const About = (props: Props) => {
     triggerOnce: true, // Trigger animation only once
     threshold: 0.2
   });
+
+  useEffect(() => {
+    gsap.to('.about-text', {
+      xPercent: 50,
+      ease: 'none',
+      scrollTrigger: {
+        trigger: '.about-text',
+        scrub: true
+      }
+    })
+    gsap.to('.about-image', {
+      xPercent: -50,
+      ease: 'none',
+      scrollTrigger: {
+        trigger: '.about-image',
+        scrub: true
+      }
+    })
+  }, [])
+
   const data = useStaticQuery(graphql`
     query {
       allContentfulTextImageDescription {
@@ -43,10 +66,10 @@ const About = (props: Props) => {
     <ComponentWrapper>
       {aboutEntries.map(({ node }) => (
         <div ref={ref} className={`lg:grid grid-cols-2 gap-4 ${inView ? 'transform translate-y-0 transition-all opacity-100' : 'transform translate-y-1/4 opacity-0'} duration-500 ease-in-out`}>
-          <div className='w-40 h-40 lg:w-80 lg:h-80 mb-4 mx-auto rounded-full overflow-hidden'>
+          <div className='w-40 h-40 lg:w-80 lg:h-80 mb-4 mx-auto rounded-full overflow-hidden about-image'>
             <img className='transition transform translate-y-0 hover:-translate-y-4 hover:cursor-pointer' src={node.image.file.url} alt="" />
           </div>
-          <div className='my-auto flex flex-col items-center'>
+          <div className='my-auto flex flex-col items-center about-text'>
             <h2 className='text-xl md:text-2xl lg:text-4xl'>{node.title}</h2>
             <h3 className='text-sm md:text-xl my-4'>{node.subTtile}</h3>
             {renderRichText(node.description, options)}
