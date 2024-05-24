f438ddfd4cf1599af7af4ca6f826facf3235e0c8
bibhor shrestha <bibhorshrestha@gmail.com>
2024-05-24 17:20:30 +0300

    add context (#17)

--- src/pages/finnish/index.tsx
+++ src/pages/finnish/index.tsx
@@ -1,14 +1,31 @@
 import React from 'react'
 import Layout from '../../components/layout'
 import Hero from '../../components/hero'
-import About from '../../components/about'
-import { graphql } from 'gatsby'
+import { PageProps, graphql } from 'gatsby'
 import ComponentWrapper from '../../components/componentWrapper'
+import { UIContextProvider } from '../../context/uiContext'
 
-const Index = ({ data }) => {
+interface MarkdownProps {
+  frontmatter: {
+    title: string;
+    description: string;
+  }
+  html: HTMLParagraphElement;
+}
+
+interface IndexPageProps extends PageProps {
+  data: {
+    allMarkdownRemark: {
+      nodes: MarkdownProps[];
+    };
+  };
+}
+
+const Index : React.FC<IndexPageProps> = ({ data }) => {
   const heroImage = {"file": { "url": "//images.ctfassets.net/jmqpmhd6e4pl/1Mm2UYwgshJErJPc4zJZ7x/3bb486e9a64c427617c3432f7cc3a191/hero-image-2.jpg"}}
   const response = data.allMarkdownRemark.nodes[1]
   return (
+    <UIContextProvider>
     <Layout>
       <Hero
         title={response.frontmatter.title}
@@ -19,6 +36,7 @@ const Index = ({ data }) => {
         <div className='markdown' dangerouslySetInnerHTML={{__html: response.html}}></div>
       </ComponentWrapper>
     </Layout>
+    </UIContextProvider>
   )
 }
 
