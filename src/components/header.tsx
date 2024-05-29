0bf659027f8553799f210528ed1dadb62a9fe7cb
bishre <bibhorshrestha@gmail.com>
2024-05-29 20:15:11 +0300

    Add logo svg

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,15 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import NavBar from "./navBar"
+import Logo from "../icons/logo"
 
 interface Props {
   siteTitle: string
 }
 const Header = ({ siteTitle }: Props) => (
-  <header className="absolute top-0 z-10 grid grid-cols-2 w-full invisible">
-    {siteTitle}
+  <header className="absolute top-0 z-10 grid grid-cols-2 w-full">
+    <h1 className="hidden">{siteTitle}</h1>
+    <Logo className="p-4" />
   </header>
 )
 
