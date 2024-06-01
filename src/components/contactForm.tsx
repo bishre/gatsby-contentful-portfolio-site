bcc3ea6e5f71d5fb3e714f7a0299b2876d9292cc
bibhor shrestha <bibhorshrestha@gmail.com>
2024-06-01 12:10:14 +0300

    Form styles (#24)

--- src/components/contactForm.tsx
+++ src/components/contactForm.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import ComponentWrapper from './componentWrapper';
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
@@ -16,20 +17,22 @@ const ContactForm = () => {
   }
 
   return (
-    <form method="post" netlify-honeypot="bot-field" data-netlify="true" name="contact">
-      <input type="hidden" name="bot-field" />
-      <input type="hidden" name="form-name" value="contact" />
-      <label>
-        Name: <input className='bg-transparent border' type="text" name="name" required value={formData.name} onChange={handleChange}/>
-      </label>
-      <label>
-        Email: <input className='bg-transparent border' type="email" name="email" value={formData.email} required onChange={handleChange}/>
-      </label>
-      <label>
-        Message: <textarea className='bg-transparent border' name="message" value={formData.message} required onChange={handleChange}></textarea>
-      </label>
-      <button type="submit">Send</button>
-    </form>
+    <ComponentWrapper>
+      <form className='flex flex-col' method="post" netlify-honeypot="bot-field" data-netlify="true" name="contact">
+        <input type="hidden" name="bot-field" />
+        <input type="hidden" name="form-name" value="contact" />
+        <label className='flex justify-between'>
+          Name: <input className='bg-transparent border w-2/3 mb-4' type="text" name="name" required value={formData.name} onChange={handleChange}/>
+        </label>
+        <label className='flex justify-between'>
+          Email: <input className='bg-transparent border w-2/3 mb-4' type="email" name="email" value={formData.email} required onChange={handleChange}/>
+        </label>
+        <label className='flex justify-between'>
+          Message: <textarea className='bg-transparent border w-2/3 mb-4' name="message" value={formData.message} required onChange={handleChange}></textarea>
+        </label>
+        <button className='border w-32 mx-auto' type="submit">Send</button>
+      </form>
+    </ComponentWrapper>
 )};
 
 export default ContactForm;
