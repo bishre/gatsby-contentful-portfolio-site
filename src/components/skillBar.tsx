182b1edff93c0acdf25fb03eaf2e058c4a1a783e
bishre <bibhorshrestha@gmail.com>
2024-06-04 15:57:21 +0300

    style fix

--- src/components/skillBar.tsx
+++ src/components/skillBar.tsx
@@ -11,10 +11,11 @@ const SkillBar = ({ skill, span }: Props) => {
     triggerOnce: true, // Trigger animation only once
     threshold: 1
   });
+  const className = `col-span-${span}`
   return (
-    <div ref={ref} className="grid grid-cols-12 grid-rows-2">
+    <div ref={ref} className="grid grid-cols-12">
       <h2 className="col-start-1 col-span-12 lg:col-span-6 text-left text-sm pb-2">{skill}</h2>
-      <div className={`row-start-2 col-span-${span} ${inView ? 'w-full' : 'w-0'} transition-all duration-500 bg-gradient-to-r from-white to-purple-500 h-4 rounded-lg`}></div>
+      <div className={`row-start-2 ${className} ${inView ? 'w-full' : 'w-0'} transition-all duration-500 bg-gradient-to-r from-white to-purple-500 h-4 rounded-lg`}></div>
     </div>
   )
 }
